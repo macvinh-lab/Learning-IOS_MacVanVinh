e73817c80aa15661df174ad0bd60307a43772874
macvinh-lab <macvinh992@gmail.com>
2026-01-06 20:06:44 +0700

    Bài 4 - Xử lý vòng đời đơn hàng

--- enumBai3.playground/Contents.swift
+++ enumBai3.playground/Contents.swift
@@ -9,18 +9,19 @@ enum ShippingMethod {
 
 // func calculateShippingFee
 func calculateShippingFee (method : ShippingMethod, orderAmount : Double?) {
-    guard let orderAmount = orderAmount else {
+    guard let orderAmount = orderAmount, orderAmount > 0 else {
         return print("Không được để orderAmount trống!!")
     }
+    
     switch method {
     case .standard :
         print("Phí giao hàng của bạn là \(orderAmount)")
     case .express :
-        print("Phí giao hàng của bạn là: \(orderAmount)")
+        print("Phí giao hàng của bạn là: \(orderAmount * 2)")
     case .sameDay(let km) :
         print("Phí giao hàng của bạn là \(orderAmount) x \(km)km = \(orderAmount*Double(km))")
     }
 }
 
 //=======
-calculateShippingFee(method: .sameDay(distanceKm: 4), orderAmount: nil)
+calculateShippingFee(method: .express, orderAmount: 1)
